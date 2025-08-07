1262dec0660def765877d5e90a3757ef3f94f327
Barrios Andres <ambarrios435@gmail.com>
2025-08-06 20:42:13 -0500

    Añadir Funcionalidad para marcar tareas como completada

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from task_model import TaskModel
 def main():
   task = TaskModel("Estudiar para el examen")
   print(f"Tarea creada: {task.get_task_name()}")
+  task.mark_as_completed()
+  print(f"Tarea completada: {task.is_task_completed()}")
 
 if __name__ == "__main__":
   main()
--- task_model.py
+++ task_model.py
@@ -1,7 +1,13 @@
 class TaskModel:
-  def __init__(self, task_name):
-    self.task_name = task_name
+    def __init__(self, task_name):
+        self.task_name = task_name
+        self.is_completed = False
 
-  def get_task_name(self):
-    return self.task_name
-    
+    def get_task_name(self):
+        return self.task_name
+
+    def mark_as_completed(self):
+        self.is_completed = True
+
+    def is_task_completed(self):
+        return self.is_completed
